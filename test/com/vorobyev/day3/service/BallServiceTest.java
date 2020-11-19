f59fefedd5cf9fbdfba37d299f1c3d39c044a44e
Vorobeyyyyyy <mrskilkchat@gmail.com>
2020-11-19 10:09:55 +0300

    day 3 test fix

--- test/com/vorobyev/day3/service/BallServiceTest.java
+++ test/com/vorobyev/day3/service/BallServiceTest.java
@@ -3,6 +3,8 @@ package com.vorobyev.day3.service;
 import com.vorobyev.day3.entity.Ball;
 import com.vorobyev.day3.entity.BallException;
 import com.vorobyev.day3.entity.Basket;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.awt.*;
@@ -10,10 +12,11 @@ import java.awt.*;
 import static org.testng.Assert.*;
 
 public class BallServiceTest {
-
     BallService service = new BallService();
-    @Test
-    public void testBasketWeight() {
+
+    Basket basket;
+    @BeforeTest
+    public void initBasket(){
         Ball[] balls = new Ball[0];
         try {
             balls = new Ball[]  {
@@ -22,10 +25,14 @@ public class BallServiceTest {
                     new Ball(56, Color.RED)
             };        }
         catch (BallException ignored){};
-        Basket basket = new Basket();
+        basket = new Basket();
         for(Ball ball : balls){
             basket.addBall(ball);
         }
+    }
+
+    @Test
+    public void testBasketWeight() {
         int expected = 22 + 14 + 56;
         int actual = service.basketWeight(basket);
         assertEquals(actual, expected);
@@ -33,20 +40,6 @@ public class BallServiceTest {
 
     @Test
     public void testCountColoredBalls() {
-        Ball[] balls = new Ball[0];
-        try {
-            balls = new Ball[]  {
-                    new Ball(22, Color.BLACK),
-                    new Ball(14, Color.BLUE),
-                    new Ball(56, Color.RED)
-            };
-        }
-        catch (BallException ignored){};
-        Basket basket = new Basket();
-        for(Ball ball : balls){
-            basket.addBall(ball);
-        }
-
         int expected = 1;
         int actual = service.countColoredBalls(basket, Color.BLUE);
         assertEquals(actual, expected);
